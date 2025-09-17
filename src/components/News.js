626a2a74dee518b1c2bd93ef1a6bf2bcd246bd67
parthjuneja040 <parthjuneja040@gmail.com>
2025-09-17 19:24:50 +0530

    Revert "Fix undefined articles array error"
    This reverts commit fa6b95748ab69448c11f6bc643dd2a4ee452e728.

--- src/components/News.js
+++ src/components/News.js
@@ -41,8 +41,8 @@ export class News extends Component {
         this.props.setProgress(50);
         console.log(parsedData);
         this.setState({
-            articles: parsedData.articles || [],
-            totalResults: parsedData.totalResults || 0,
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
             loading: false
         })
         this.props.setProgress(100);
@@ -55,8 +55,8 @@ export class News extends Component {
             let parsedData = await data.json()
             console.log(parsedData);
             this.setState({
-                articles: this.state.articles.concat(parsedData.articles || []),
-                totalResults: parsedData.totalResults || 0,
+                articles: this.state.articles.concat(parsedData.articles),
+                totalResults: parsedData.totalResults,
                 loading: false
             })
             console.log("Articles");
